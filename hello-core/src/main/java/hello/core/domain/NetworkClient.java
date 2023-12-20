d1cac5fc99eebe52f337bcb422fe8d55bdee67ab
jiseong0612 <hth5545@gmail.com>
2023-12-20 10:47:26 +0900

    @Bean(initMethod = "init", destroyMethod = "close") 설정 정보에 초기화 소멸 메서드 지정

--- hello-core/src/main/java/hello/core/domain/NetworkClient.java
+++ hello-core/src/main/java/hello/core/domain/NetworkClient.java
@@ -1,16 +1,10 @@
 package hello.core.domain;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-//인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고, 지금은 다음의 더 나은 방법들이 있어서 거의 사용하지 않는다.
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 	private String url;
 
 	public NetworkClient() {
 		System.out.println("생성자 호출, url = " + url);
-		connect();
-		call("초기화 연결 메시지");
 	}
 
 	public void setUrl(String url) {
@@ -27,19 +21,18 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 	}
 
 	// 서비스 종료시 호출
-	public void disconnect() {
-		System.out.println("close: " + url);
+	public void disConnect() {
+		System.out.println("close + " + url);
 	}
 
-	@Override
-	public void destroy() throws Exception {
-		disconnect();
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
+	public void init() {
+		System.out.println("NetworkClient.init");
 		connect();
-		call("afterPropertiesSet__초기화 연결 메시지");
+		call("초기화 연결 메시지");
+	}
 
+	public void close() {
+		System.out.println("NetworkClient.close");
+		disConnect();
 	}
 }
