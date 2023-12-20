c8f4afd4340e7020a409ba0b9843606d1f8a6e95
jiseong0612 <hth5545@gmail.com>
2023-12-20 11:31:51 +0900

    request 스코프 예제 만들기

---
+++ hello-core/src/main/java/hello/core/controller/LogDemoController.java
@@ -0,0 +1,30 @@
+package hello.core.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import hello.core.common.MyLogger;
+import hello.core.service.LogDemoService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+	private final LogDemoService logDemoService;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
+
+	@RequestMapping("log-demo")
+	@ResponseBody
+	public String logDemo(HttpServletRequest request) {
+		String requestURL = request.getRequestURL().toString();
+		MyLogger myLogger = myLoggerProvider.getObject();
+		myLogger.setRequestURL(requestURL);
+		myLogger.log("controller test");
+		logDemoService.logic("testId");
+		return "OK";
+	}
+}
