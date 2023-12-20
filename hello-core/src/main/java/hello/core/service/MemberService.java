d05603c4012cda6fddc49672ac83bf3966a201fa
jiseong0612 <hth5545@gmail.com>
2023-12-20 09:56:30 +0900

    롬복 @RequiredArgsConstructor를 이용한 의존관계 자동 주입

--- hello-core/src/main/java/hello/core/service/MemberService.java
+++ hello-core/src/main/java/hello/core/service/MemberService.java
@@ -1,19 +1,15 @@
 package hello.core.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import hello.core.domain.Member;
 import hello.core.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class MemberService {
 	private final MemberRepository memberRepository;
-	
-	@Autowired
-	public MemberService(MemberRepository memberRepository) {
-		this.memberRepository = memberRepository;
-	}
 	
 	public void join(Member member) {
 		memberRepository.save(member);
--- hello-core/src/main/java/hello/core/service/OrderService.java
+++ hello-core/src/main/java/hello/core/service/OrderService.java
@@ -1,24 +1,19 @@
 package hello.core.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.domain.Member;
 import hello.core.domain.Order;
 import hello.core.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class OrderService {
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 	
-	@Autowired
-	public OrderService(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-		this.memberRepository = memberRepository;
-		this.discountPolicy = discountPolicy;
-	}
-
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
 		Member member = memberRepository.findById(memberId);
 		
